9c43dfabb6fb07be1470fa50c321fa05759110da
Alexander Voelz <alexandervoelz@icloud.com>
2021-03-25 11:28:50 +0100

    Initial Commit

---
+++ AlignmentGuides/AlignmentGuidesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlignmentGuidesApp.swift
+//  AlignmentGuides
+//
+//  Created by Alexander Völz on 25.03.21.
+//
+
+import SwiftUI
+
+@main
+struct AlignmentGuidesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlignmentGuides/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlignmentGuides
+//
+//  Created by Alexander Völz on 25.03.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
